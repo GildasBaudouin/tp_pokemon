9bae020e0439b6b5e893f6de9185dc86ca53788b
gbaudouin2022 <gildas.baudouin2022@campus-eni.fr>
2023-03-17 15:27:54 +0100

    1 commit

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -8,24 +8,27 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 #[Route('pokemon/', name: 'pokemon_')]
 class PokemonController extends AbstractController
 {
     #[Route('list/{tri}', name: 'list')]
     public function list(PokemonRepository $pokemonRepository, $tri = null): Response
     {
-        $pokemonList = $pokemonRepository ->triPokemon($tri);
+        $pokemonList = $pokemonRepository->triPokemon($tri);
+//        $pokemonList = $pokemonRepository->findBy([],['']);
+
 //        dump($pokemonList);
         return $this->render('main/list.html.twig', [
-         'pokemonList' => $pokemonList
-    ]);
+            'pokemonList' => $pokemonList
+        ]);
     }
 
 
     #[Route('details/{id}', name: 'details')]
     public function details(int $id, PokemonRepository $pokemonRepository): Response
     {
-        $pokemon = $pokemonRepository ->find($id);
+        $pokemon = $pokemonRepository->find($id);
         return $this->render('pokemon/details.html.twig', [
             'pokemon' => $pokemon
         ]);
@@ -34,20 +37,20 @@ class PokemonController extends AbstractController
     #[Route('capture/{id}', name: 'capture')]
     public function capture(int $id, EntityManagerInterface $entityManager, PokemonRepository $pokemonRepository): Response
     {
-        $pokemon = $pokemonRepository -> find($id);
-        $capture = $pokemon ->isEstCapture();
+        $pokemon = $pokemonRepository->find($id);
+        $capture = $pokemon->isEstCapture();
 
-        if ($capture) {
-            $pokemon ->setEstCapture(false);
-        } else {
-            $pokemon ->setEstCapture(true);
-        }
+        $capture ?  $pokemon->setEstCapture(false) :   $pokemon->setEstCapture(true);
+
+//        if ($capture) {
+//            $pokemon->setEstCapture(false);
+//        } else {
+//            $pokemon->setEstCapture(true);
+//        }
 
         $entityManager->flush();
-        $pokemonList = $pokemonRepository ->findAll();
+        $pokemonList = $pokemonRepository->findAll();
 
-        return $this->render('main/list.html.twig', [
-            'pokemonList' => $pokemonList
-        ]);
+        return $this->redirectToRoute('pokemon_list');
     }
 }
